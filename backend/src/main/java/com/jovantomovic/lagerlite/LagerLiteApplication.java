843694adfdf7f18ea9ffc10c113988594e6668e8
Tomovic <jovantomovic5@gmail.com>
2025-06-29 16:27:22 +0200

    spring boot init

---
+++ backend/src/main/java/com/jovantomovic/lagerlite/LagerLiteApplication.java
@@ -0,0 +1,13 @@
+package com.jovantomovic.lagerlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagerLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LagerLiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jovantomovic/lagerlite/LagerLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovantomovic.lagerlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagerLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
